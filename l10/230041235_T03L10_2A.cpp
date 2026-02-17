e1beab5071a159039d06f8d61dc93ebc6a57539b
Yakiyo <yakiyo1206@gmail.com>
2026-02-17 21:23:03 +0600

    feat: fix

--- l10/230041235_T03L10_2A.cpp
+++ l10/230041235_T03L10_2A.cpp
@@ -2,33 +2,33 @@
 #include<vector>
 using namespace std;
 
-vector<vector<int>> adj_list;
-vector<vector<int>> adj_matrix;
+vector<vector<int>> glist;
+vector<vector<int>> gmatx;
 
 void insert(int u, int v){
-    adj_list[u-1].push_back(v);
-    adj_list[v-1].push_back(u);
-    adj_matrix[u-1][v-1] = 1;
-    adj_matrix[v-1][u-1] = 1;
+    glist[u-1].push_back(v);
+    glist[v-1].push_back(u);
+    gmatx[u-1][v-1] = 1;
+    gmatx[v-1][u-1] = 1;
 }
 
-void list_print(int v){
+void display_list(int v){
     cout << "Adjacency List:" << endl;
     for(int i=0; i<v; i++){
         cout << i+1 << ": ";
-        int size = adj_list[i].size();
+        int size = glist[i].size();
         for(int j=0; j<size; j++){
-            cout << adj_list[i][j] << " ";
+            cout << glist[i][j] << " ";
         }
         cout << endl;
     }
 }
 
-void mat_print(int v){
+void display_matx(int v){
     cout << "Adjacency Matrix:" << endl;
     for(int i=0;i<v;i++){
         for(int j=0;j<v;j++){
-            int temp = (adj_matrix[i][j] == 1) ? 1 : 0;
+            int temp = (gmatx[i][j] == 1) ? 1 : 0;
             cout << temp << " ";
         }
         cout << endl;
@@ -36,18 +36,18 @@ void mat_print(int v){
 }
 
 int main(){
-    int vertices, edges;
-    cin >> vertices >> edges;
+    int vn, en;
+    cin >> vn >> en;
 
-    adj_list.resize(vertices);
-    adj_matrix.assign(vertices, vector<int>(vertices, 0));
+    glist.resize(vn);
+    gmatx.assign(vn, vector<int>(vn, 0));
 
     int input1, input2;
-    for(int i=0; i<edges; i++){
+    for(int i=0; i<en; i++){
         cin >> input1 >> input2;
         insert(input1, input2);
     }
 
-    list_print(vertices);
-    mat_print(vertices);
+    display_list(vn);
+    display_matx(vn);
 }
--- l10/230041235_T04L10_2A.cpp
+++ l10/230041235_T04L10_2A.cpp
@@ -1,69 +1,68 @@
-#include<iostream>
-#include<vector>
-#include<queue>
+#include <iostream>
+#include <queue>
+#include <vector>
 using namespace std;
 
-vector<vector<int>> adj_list;
+vector<vector<int>> glist;
 vector<int> indeg;
 
-
-void insert(int u, int v){
-    adj_list[u].push_back(v);
-    adj_list[v].push_back(u);
+void insert(int u, int v) {
+    glist[u].push_back(v);
+    glist[v].push_back(u);
     indeg[v]++;
 }
 
-vector<int> topo_sort(int vertices){
-
+vector<int> topo_sort(int vertices) {
     vector<int> Ans;
     queue<int> Que;
-    for(int i=0; i<vertices; i++){
-        if(indeg[i] == 0){ Que.push(i);}
+    for (int i = 0; i < vertices; i++) {
+        if (indeg[i] == 0) {
+            Que.push(i);
+        }
     }
-    if(Que.empty()){
+    if (Que.empty()) {
         cout << "[ ]" << endl;
-        return vector<int> (0);
+        return vector<int>(0);
     }
 
-    while(!Que.empty()){
-        int curr;
-        curr = Que.front();
+    while (!Que.empty()) {
+        int curr = Que.front();
         Que.pop();
         Ans.push_back(curr);
-        
-        int temp = adj_list[curr].size();
-        for(int i=0; i<temp; i++){
-            int ind = adj_list[curr][i];
-            indeg[ind]--;
-            if(indeg[ind] == 0) Que.push(ind);
-        }
 
+        int temp = glist[curr].size();
+        for (int i = 0; i < temp; i++) {
+            int ind = glist[curr][i];
+            indeg[ind]--;
+            if (indeg[ind] == 0) Que.push(ind);
+        }
     }
 
-    if(Ans.size() != vertices){
+    if (Ans.size() != vertices) {
         cout << "[ ]" << endl;
-        return vector<int> (0);
+        return vector<int>(0);
     }
     return Ans;
 }
 
-int main(){
+int main() {
     int vertices, edges;
     cin >> vertices >> edges;
 
-    adj_list.resize(vertices);
+    glist.resize(vertices);
     indeg.assign(vertices, 0);
 
-
-    int input1, input2;
-    for(int i=0; i<edges; i++){
-        cin >> input1 >> input2;
-        insert(input1, input2);
+    int a, b;
+    for (int i = 0; i < edges; i++) {
+        cin >> a >> b;
+        insert(a, b);
     }
 
     vector<int> Ans = topo_sort(vertices);
-    if(!Ans.empty()){
-        for(int i=0; i<vertices; i++){cout << Ans[i] << " ";}
+    if (!Ans.empty()) {
+        for (int i = 0; i < vertices; i++) {
+            cout << Ans[i] << " ";
+        }
         cout << endl;
     }
 }
